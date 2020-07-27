5b3fb9967ef9fa0349cb64041204b7dec22380e9
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-07-27 22:54:06 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ struct ASGIResponse {
 fn main() {    
 
     // some stuff
-    let threads: usize = 3;
+    let threads: usize = 1;
 
     let host: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
     let port: u16 = 8080;
@@ -300,11 +300,7 @@ async fn get_resp(
     }
 
     let mut id_: usize = 1;
-    if sys_id % 2 == 0 {
-        id_ = 2;
-    } else if sys_id % 3 == 0 {
-        id_ = 3;
-    } 
+
 
     let sys_id = sys_id.to_string();
 
