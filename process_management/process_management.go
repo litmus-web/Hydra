c368ce5ab1e989ee9b458a954ffe870d33a561d6
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-07-30 00:12:17 +0100

    Update process_management.go

--- process_management/process_management.go
+++ process_management/process_management.go
@@ -1,6 +1,7 @@
 package process_management
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -19,31 +20,51 @@ var newProcesses = ActiveWorkers{}
 type TargetApp struct {
 	Target string
 	App    string
+	Auth   string
+}
+
+type TargetWorker struct {
+	Target string
+	App    string
 	Id     string
 	Auth   string
 }
 
 func StartWorkers(amountProcesses int, target TargetApp) {
-	println("Waiting for server to finish loading before booting workers")
+	log.Println("Waiting for server to finish loading before booting workers")
 	time.Sleep(500 * time.Millisecond)
-	println("Starting Workers...")
-	for i := 0; i <= amountProcesses; i++ {
-		spawnedWorker := startWorker(target)
+	log.Println("Starting Workers...")
+	for i := 1; i <= amountProcesses; i++ {
+		workerCfg := TargetWorker{
+			Target: target.Target,
+			App:    target.App,
+			Id:     fmt.Sprintf("%v", i),
+			Auth:   target.Auth,
+		}
+		spawnedWorker := startWorker(workerCfg)
 		go handleNewWorker(spawnedWorker)
 	}
 
-	println("Starting worker watcher")
+	log.Println("Starting worker watcher")
 	for {
 		newProcesses := ActiveWorkers{}
 		for i, worker := range activeProcesses.workers {
-			if worker.ProcessState.Exited() {
-				log.Println(
-					"Worker ", i,
-					" has exited with code: ", worker.ProcessState.ExitCode(),
-					" attempting restart.")
+			if worker.ProcessState != nil {
+				if worker.ProcessState.Exited() {
+					log.Println(
+						"Worker ", i,
+						" has exited with code: ", worker.ProcessState.ExitCode(),
+						" attempting restart.")
 
-				spawnedWorker := startWorker(target)
-				go handleRestartWorker(spawnedWorker)
+					workerCfg := TargetWorker{
+						Target: target.Target,
+						App:    target.App,
+						Id:     fmt.Sprintf("%v", i),
+						Auth:   target.Auth,
+					}
+					spawnedWorker := startWorker(workerCfg)
+					go handleRestartWorker(spawnedWorker)
+				}
 			} else {
 				newProcesses.workers = append(newProcesses.workers, worker)
 			}
@@ -53,7 +74,7 @@ func StartWorkers(amountProcesses int, target TargetApp) {
 	}
 }
 
-func startWorker(target TargetApp) *exec.Cmd {
+func startWorker(target TargetWorker) *exec.Cmd {
 	spawnedWorker := exec.Command("python", target.Target, target.App, target.Id, target.Auth)
 	spawnedWorker.Stdout = os.Stdout
 	spawnedWorker.Stderr = os.Stdout
@@ -66,7 +87,7 @@ func handleNewWorker(worker *exec.Cmd) {
 		log.Fatal("Error starting Worker\nError Message: ", err)
 	} else {
 		time.Sleep(200 * time.Millisecond)
-		println("Started worker with Pid: ", worker.Process.Pid)
+		log.Println("Started worker with Pid: ", worker.Process.Pid)
 		mutex.Lock()
 		activeProcesses.workers = append(activeProcesses.workers, *worker)
 		mutex.Unlock()
@@ -79,7 +100,7 @@ func handleRestartWorker(worker *exec.Cmd) {
 		panic("Unable to start worker process")
 	} else {
 		time.Sleep(200 * time.Millisecond)
-		println("Started worker with Pid: ", worker.Process.Pid)
+		log.Println("Started worker with Pid: ", worker.Process.Pid)
 		mutex.Lock()
 		newProcesses.workers = append(newProcesses.workers, *worker)
 		mutex.Unlock()
