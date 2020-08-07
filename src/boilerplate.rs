32c5d09790eb85e2d06e2ba550f4e7353fa1f7e4
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-08-07 18:28:07 +0100

    moving boiler plate to not start threads and be process only

--- src/boilerplate.rs
+++ src/boilerplate.rs
@@ -3,27 +3,7 @@ use std::thread;
 pub mod server;
 
 
-#[derive(Clone)]
-pub struct Target {
-    pub file_path: String,
-    pub app: String,
-}
-
-pub fn create_forks(amount_of_processes: usize, target: Target, server_config: server::Config) {
-    let workers = server::Workers::default();
-
-    for i in 1..amount_of_processes {
-        let t = target.clone();
-        let c = server_config.clone();
-        let w = workers.clone();
-        let _ = thread::Builder::new().name(format!("fork_{}", i).to_string()).spawn(move || {
-            create_boilerplate(i, t, c, w)
-        });
-    }
-    create_boilerplate(0, target.clone(), server_config.clone(), workers.clone())
-}
-
-fn create_boilerplate(process_id: usize, target: Target, server_config: server::Config, workers: server::Workers) {
+pub fn create_boilerplate(process_id: usize, server_config: server::Config) {
      // Configure a runtime that runs everything on the current thread
      let mut rt = tokio::runtime::Builder::new()
      .enable_all()
@@ -37,8 +17,7 @@ fn create_boilerplate(process_id: usize, target: Target, server_config: server::
         &mut rt,
         server::run(
             process_id,
-            server_config,
-            workers
+            server_config,        
         )
     );
 }
