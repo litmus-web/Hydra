5a674b15ae54e73b4e3cb15b52f448d22e8cae0c
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-08-07 18:28:17 +0100

    single threaded pre forking

--- src/boilerplate/server.rs
+++ src/boilerplate/server.rs
@@ -1,109 +1,198 @@
-use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::collections::HashMap;
-use std::cell::Cell;
 use std::rc::Rc;
+use std::cell::{Cell, RefCell};
 use std::io;
 use std::convert::Infallible;
 use std::str;
 
 use futures::prelude::*;
-use futures::stream::StreamExt;
+use futures::{FutureExt, StreamExt};
+
+use futures_util::future::join;
 
 use headers::{self, HeaderMapExt};
 
 use hyper::header::{self, AsHeaderName, HeaderMap, HeaderValue};
 use hyper::server::Server;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{self, Body, Method, Request, Response, StatusCode};
+use hyper::{self, Body, Request, Response, StatusCode};
+use hyper::http::Error;
 
-use tokio::sync::RwLock;
 use tokio::net::TcpListener;
+use tokio::sync::mpsc;
 
 use tokio_tungstenite::{tungstenite::protocol, WebSocketStream};
 
-pub type Workers = Arc<RwLock<HashMap<String, String>>>;
+use serde_json;
+use serde::{Serialize, Deserialize};
 
+///
+///  Static globals
+///
+static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
+
+///
+///  Custom types for workers, cache, etc...
+/// 
+///     - Workers: Set in a HashMap contained in a RC to allow us to use Default().
+///     - Thread Safety: This is fine because the system is Single threaded.
+type Workers = Rc<HashMap<String, Result<mpsc::UnboundedSender<protocol::Message>, Error>>>;
+type Cache = RefCell<HashMap<String, ASGIResponse>>;
+
+
+///
+///  General Structs for communication between systems.
+///
 #[derive(Clone)]
 pub struct Config {
-    pub addr: &'static str,
+    pub addr: String,
     pub port: u16,
 }
 
-pub async fn run(process_id: usize, server_config: Config, workers: Workers)  { 
-    println!(
-        "[ WORKER {} ] Worker starting...", process_id);
-    
-    let _ = run_server(process_id, server_config, workers).await;
+#[derive(Clone)]
+struct OutGoingRequest {
+    request_id: usize,
+    method: String,
+    remote: String,
+    path: String,
+    headers: HashMap<String, String>,
 }
 
-async fn run_server(process_id: usize, server_config: Config, workers: Workers) -> io::Result<()> {
+#[derive(Clone, Serialize, Deserialize)]
+struct ASGIResponse {
+    request_id: String,
+    status: u16,
+    headers: HashMap<String, String>,
+    body: String
+}
 
-    let listener = TcpListener::bind(format!("{}:{}", server_config.addr, server_config.port)).await?;
 
-    // Using a !Send request counter is fine on 1 thread...
-    let counter: std::rc::Rc<std::cell::Cell<usize>> = Rc::new(Cell::new(0));
 
-    let make_service = make_service_fn(move |_| {
-        // For each connection, clone the counter to use in our service...
-        let cnt = counter.clone();
+///
+///  Runners for main function.
+///
+pub async fn run(target_id: usize, server_config: Config)  {     
+    let _ = run_server(target_id, server_config).await;
+}
+
+async fn run_server(target_id: usize, server_config: Config) -> io::Result<()> {
+    // Generating ref cells to allow us to move data around.
+    let workers = Workers::default();
+    let cache = Cache::default();
+
+    let worker_lock = workers.clone();
+    let cache_lock = cache.clone();
+    
+    // Server binds, `?` makes the binder attach with SO_REUSEADDR, using unwrap() keeps the ports individual.
+    let main_listener = TcpListener::bind(format!("{}:{}", server_config.addr, server_config.port)).await?;
+    let worker_listener = TcpListener::bind(format!("{}:{}", server_config.addr, target_id)).await.unwrap();
+
+    // Server service functions
+    let main_service = make_service_fn(move |_| {
+        let main_workers_clone = worker_lock.clone();
+        let main_cache_clone = cache_lock.clone();
 
         async {
             Ok::<_, Infallible>(service_fn(move |req| {
-                handle_incomming(req)
+                handle_incoming(req, main_workers_clone.clone(), main_cache_clone.clone())
             })) 
         }
     });
+
+    let worker_service = make_service_fn(move |_| {
+        let worker_workers_clone = workers.clone();
+        let worker_cache_clone = cache.clone();
+
+        async {
+            Ok::<_, Infallible>(service_fn(move |req| {
+                handle_workers_incoming(req, worker_workers_clone.clone(), worker_cache_clone.clone())
+            })) 
+        }
+    });
+
     
-    let server = Server::builder(hyper::server::accept::from_stream(listener))
+    // Generating server builds and executors
+    let server_1 = Server::builder(hyper::server::accept::from_stream(main_listener))
         .executor(LocalExec)
         .http1_pipeline_flush(true)
-        .serve(make_service)
-        .await;
+        .serve(main_service);
 
-    if let Err(err) = server {
-        eprintln!("Error when starting {:?}", err)
-    };
+    let server_2 = Server::builder(hyper::server::accept::from_stream(worker_listener))
+        .executor(LocalExec)
+        .http1_pipeline_flush(true)
+        .serve(worker_service);
+
+    let _ = join(server_1, server_2).await;
+
     Ok(())
 }
 
 
-#[derive(Clone, Copy, Debug)]
-struct LocalExec;
 
-impl<F> hyper::rt::Executor<F> for LocalExec
-where
-    F: std::future::Future + 'static, // not requiring `Send`
-{
-    fn execute(&self, fut: F) {
-        tokio::task::spawn_local(fut);
-    }
-}
+///
+///  Main area where all incoming requests get sent.
+///
+async fn handle_incoming(req: Request<Body>, workers: Workers, cache: Cache) -> Result<Response<Body>, Error> {
+    let req_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
 
-async fn handle_incomming(req: Request<Body>) -> Result<Response<Body>, io::Error> {
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/workers") => Ok(handle_worker(req).await),
-        _ => Ok(handle_any(req).await),
-    }
-}
+    let outgoing = OutGoingRequest {
+        request_id: req_id,
+        method: req.method().to_string(),
+        remote: String::from(req.uri().host().unwrap()),
+        path: req.uri().path().to_string(),
+        headers: Default::default(),
+    };
 
-async fn handle_any(req: Request<Body>) -> Response<Body> {
-    Response::builder()
+
+    Ok(
+        Response::builder()
         .status(StatusCode::NOT_FOUND)
         .body("Not Found".into())
         .unwrap()
+    )
 }
 
-async fn handle_worker(req: Request<Body>) -> Response<Body> {
-    let w = handle_ws_connection(req).await;
+
+
+
+///
+///  This is the worker area, responsible for upgrading the WS connection
+///  to the server allowing for fast transactions between processes.
+///
+async fn handle_workers_incoming(
+    req: Request<Body>,
+    workers: Workers,
+    cache: Cache
+    ) -> Result<Response<Body>, Error> {
+
+    match req.uri().path() {
+        "/workers" => Ok(handle_worker(req, workers, cache).await),
+        _ => {
+            Ok(
+                Response::builder()
+                    .status(StatusCode::NOT_FOUND)
+                    .body("Not Found".into())
+                    .unwrap()
+            )
+        }
+    }
+}
+
+async fn handle_worker(req: Request<Body>, workers: Workers, cache: Cache) -> Response<Body> {
+    let w = handle_ws_connection(req, workers, cache).await;
     w.unwrap()
 }
 
-async fn handle_ws_connection(req: Request<Body>) -> Result<Response<Body>, io::Error> {
+async fn handle_ws_connection(
+    req: Request<Body>,
+    workers: Workers,
+    mut cache: Cache,
+    ) -> Result<Response<Body>, io::Error> {
+
     let res = match upgrade_connection(req).await {
         Err(res) => res,
         Ok((res, ws)) => {
-
-            // Create a very basic async task for a echo ws test.
             let run_ws_task = async {
                 match ws.await {
                     Ok(ws) => {
@@ -112,33 +201,39 @@ async fn handle_ws_connection(req: Request<Body>) -> Result<Response<Body>, io::
                         let mut counter: usize = 0;
 
                         // Split the ws connection into sender and reciever...
-                        let (tx, rc) = ws.split();
+                        let (ws_tx, mut ws_rc) = ws.split();
+                        let (tx, rx) = mpsc::unbounded_channel();
 
-                        // Check if the future is Ok, map that then
-                        // to a new response formatting with the message.
-                        let rc = rc.try_filter_map(|m| {
-                            future::ok(match m {
-                                protocol::Message::Text(text) => {
-                                    counter += 1;
-                                    Some(protocol::Message::text(format!(
-                                        "Response {}: {}",
-                                        counter, text
-                                    )))
-                                }
-                                _ => None,
-                            })
-                        });
+                        tokio::task::spawn_local(rx.forward(ws_tx).map(|result| {
+                            if let Err(e) = result {
+                                eprintln!("websocket send error: {}", e);
+                            }
+                        }));
+                        
+                        workers.insert(String::from("main"), tx);
 
-                        // Try forward the message onto the sender
-                        match rc.forward(tx).await {
-                            Err(e) => eprintln!("WS Error {}", e),
-                            Ok(_) => println!("Websocket has ended"),
+                        // Run it until something breaks or it stops normally.
+                        while let Some(result) = ws_rc.next().await {
+                            let msg = match result {
+                                Ok(protocol::Message::Text(text)) => text,
+                                Err(e) => {
+                                    eprintln!("websocket error {}", e);
+                                    break;
+                                },
+                                _ => String::from("None")
+                            };
+
+                            let outgoing: ASGIResponse = serde_json::from_str(msg.as_str()).unwrap();
+                            let mut borred = cache.borrow_mut();
+                            borred.insert(outgoing.clone().request_id, outgoing);
+
                         }
                     }
                     Err(_e) => eprintln!("WS error"),
                 }
+                cache
             };
-            tokio::spawn(run_ws_task);
+            tokio::task::spawn_local(run_ws_task);
             res
         }
     };
@@ -203,9 +298,30 @@ async fn upgrade_connection(req: Request<Body>) -> Result<(
         .on_upgrade()
         .map_err(|err| eprintln!("Cannot create websocket: {} ", err))
         .and_then(|upgraded| async {
-            let r = WebSocketStream::from_raw_socket(upgraded, protocol::Role::Server, None).await;
+            let r = WebSocketStream::from_raw_socket(
+                upgraded,
+                protocol::Role::Server,
+                None
+            ).await;
             Ok(r)
         });
 
     Ok((res, upgraded))
+}
+
+
+
+///
+///  Local future exectutor for hyper, cuz? Threadsafety ig.
+///
+#[derive(Clone, Copy, Debug)]
+struct LocalExec;
+
+impl<F> hyper::rt::Executor<F> for LocalExec
+where
+    F: std::future::Future + 'static, // not requiring `Send`
+{
+    fn execute(&self, fut: F) {
+        tokio::task::spawn_local(fut);
+    }
 }
