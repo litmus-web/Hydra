ab17a0f711272e2845ebc25988e04b554079eda4
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-08-16 20:33:27 +0100

    Update __init__.py

--- sandman_server/__init__.py
+++ sandman_server/__init__.py
@@ -1,7 +1,20 @@
+import argparse
+
 from .workers.websocket import AutoShardedWorker, WebsocketShard, InternalResponses
-from .workers.handlers.auto import Worker
-from .utils.helpers import find_free_port, is_child
+from .workers.workers import Worker
 from .main import run
 from .adapters.asgi import ASGIAdapter
 from .adapters.wsgi import WSGIAdapter
 from .adapters.raw import RawAdapter
+
+flags = argparse.ArgumentParser()
+flags.add_argument("--child", type=bool, required=True)
+flags.add_argument("--app", type=str, required=True)
+flags.add_argument("--adapter", type=str, required=True)
+flags.add_argument("--port", type=int, required=True)
+flags.add_argument("--shards", type=int, required=True)
+parsed = flags.parse_args()
+
+
+def is_child() -> bool:
+    return parsed.child
