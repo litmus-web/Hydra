d6d08e4eed66cb140831f0e0b879777d65f70fb6
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-09-10 22:35:24 +0100

    clearing for go

--- sandman_client/__init__.py
+++
@@ -1,9 +0,0 @@
-from .workers.websocket import AutoShardedWorker, WebsocketShard, InternalResponses
-from .workers.workers import Worker
-from .adapters.asgi import ASGIAdapter
-from .adapters.wsgi import WSGIAdapter
-from .adapters.raw import RawAdapter
-from .runner import run
-from .helpers import load_data, dumps_data
-
-
--- sandman_client/codes.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass(frozen=True)
-class OpCodes:
-    IDENTIFY = 0
-    HTTP_REQUEST = 1
-    MESSAGE = 2
